92ae5760d28522298123427b42bd744ae580fae1
Giuseppe Averta <g.averta3@gmail.com>
2024-07-14 17:49:56 +0200

    startup dao

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,23 @@
 from database.DB_connect import DBConnect
+from model.gene import Gene
 
 
 class DAO():
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def getAllGenes():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select * from genes"""
+
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(Gene(**row))
+
+        cursor.close()
+        conn.close()
+        return result
